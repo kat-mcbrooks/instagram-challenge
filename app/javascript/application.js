3fca611ed6b64c9e7f05cbf61cdb6c766bea867a
kat-mcbrooks <katharinebrooks@hotmail.co.uk>
2022-02-18 17:41:53 +0000

    generate Post scaffold

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,9 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "bootstrap"
 import "@hotwired/turbo-rails"
+import "@hotwired/stimulus-loading"
 import "controllers"
+
+import "trix"
+import "@rails/actiontext"
 
