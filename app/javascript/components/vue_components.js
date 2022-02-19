896a47fbb056be4f0083394b643c12aaef196197
kat-mcbrooks <katharinebrooks@hotmail.co.uk>
2022-02-19 13:46:42 +0000

    imported vue. playing with vue components.

---
+++ app/javascript/components/vue_components.js
@@ -0,0 +1,26 @@
+import Vue from 'vue' //importing vue from the importmapped identified 'vue'
+
+new Vue({
+  el: '#app-4',
+  data: {
+    todos: [
+      { text: 'Learn JavaScript' },
+      { text: 'Learn Vue' },
+      { text: 'Build something awesome' }
+    ]
+  }
+})
+
+new Vue({
+  el: '#app-5',
+  data: {
+    message: 'Hello Vue.js!'
+  },
+  methods: {
+    reverseMessage: function () {
+      this.message = this.message.split('').reverse().join('')
+    }
+  }
+})
+
+
--- config/importmap.rb
+++ config/importmap.rb
@@ -1,11 +1,13 @@
 # Pin npm packages by running ./bin/importmap
 
-pin "application", preload: true
+
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
 
 pin "@hotwired/stimulus", to: "stimulus.min.js", preload: true
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js", preload: true #also seen "@hotwired/stimulus-importmap-autoloader", to: "stimulus-importmap-autoloader.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
+
+pin_all_from "app/javascript/components", under: "components" 
 
 pin "jquery", to: "https://ga.jspm.io/npm:jquery@3.6.0/dist/jquery.js"
 pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@5.1.3/dist/js/bootstrap.esm.js"
@@ -15,3 +17,6 @@ pin "trix"
 pin "@rails/actiontext", to: "actiontext.js"
 
 pin 'md5', to: 'https://cdn.skypack.dev/md5'
+pin "vue", to: 'https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.esm.browser.js'
+
+pin "application", preload: true
